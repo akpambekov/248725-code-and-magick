12def71a4c239dd38dc13394fe39497f905aa04f
Ilnaz <akpambekov.rabota@yandex.ru>
2017-08-21 22:11:14 +0300

    исправил по замечаниям

--- js/stat.js
+++ js/stat.js
@@ -1,37 +1,75 @@
+'use strict';
+
+var drawContainer = function(context, containerCol, x, y, width, height, shadowCol, shadowX, shadowY) {
+  context.fillStyle = containerCol;
+  context.shadowColor = shadowCol;
+  context.shadowOffsetX = shadowX;
+  context.shadowOffsetY = shadowY;
+  context.fillRect(x, y, width, height);
+  context.shadowOffsetX = 0;
+  context.shadowOffsetY = 0;
+}
+
+var drawText = function() {
+  ctx.textBaseline = 'top';
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = 'black';
+
+}
+
+var drawText = function(context, baseline, font, fontColor, text, textX, textY) {
+  context.textBaseline = baseline;
+  context.font = font;
+  context.fillStyle = fontColor;
+  context.fillText(text, textX, textY);
+}
+
+var findMaxValueInArr = function(arr) {
+  var maxVal = -1;
+
+  for(var i = 0; i < arr.length; i++) {
+    var currentVal = arr[i];
+    if (currentVal > maxVal) {
+      maxVal = currentVal;
+    }
+  }
+
+  return maxVal;
+}
+
+var paintBar = function(context, val1, val2, color, color2) {
+  if (val1 === val2) {
+    context.fillStyle = color;
+  } else {
+    context.fillStyle = color2;
+  }
+};
+
+var drawBarAndSignature = function(context, x, y, width, indent, text, textColor, textIndent, sign, signIndent) {
+  context.fillRect(x, y, width, indent);
+  context.fillStyle = textColor;
+  context.fillText(text, x, textIndent);
+  context.fillText(sign, x, signIndent);
+};
+
 window.renderStatistics = function(ctx, names, times) {
+  var polygonColor = 'hsla(360,100%,100%,1.00)';
+  var polygonShadowColor = 'hsla(0, 0%, 0%, 1)';
   var initialX = 100;
   var initialY = 10;
   var polygonWidth = 420;
   var polygonHeight = 270;
-
-  ctx.fillStyle = "white";
-  ctx.shadowColor = "rgba(0, 0, 0, 0.7)";
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.fillRect(initialX, initialY, polygonWidth, polygonHeight);
-
+  var polygonShadowX = 10;
+  var polygonShadowY = 10;
+  var text1 = 'Ура вы победили!';
+  var text2 = 'Список результатов:';
+  var textBaseline = 'top';
+  var font = '16px PT Mono';
   var textIndentX = 60;
   var textIndentY = 20;
   var additionalTextIndentY = 20;
-  var textColor = "black";
-
-  ctx.shadowOffsetX = 0;
-  ctx.shadowOffsetY = 0;
-  ctx.textBaseline = "top";
-  ctx.font = "16px PT Mono";
-  ctx.fillStyle = textColor;
-  ctx.fillText("Ура вы победили!", initialX + textIndentX, textIndentY);
-  ctx.fillText("Список результатов:", initialX + textIndentX, textIndentY + additionalTextIndentY);
-
-  var maxTime = -1;
-
-  for(var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > maxTime) {
-      maxTime = time;
-    }
-  }
-
+  var textColor = 'black';
+  var maxTime = findMaxValueInArr(times);
   var histogramHeight = 150;
   var step = histogramHeight / maxTime;
   var indentForNameY = 20;
@@ -40,18 +78,34 @@ window.renderStatistics = function(ctx, names, times) {
   var barWidth = 40;
   var initialIndentForBar = 50;
   var additionalIndentForBar = 50 + barWidth;
+  var currentPlayer = 'Вы';
+  var currentPlayerColor = 'hsla(0, 100%, 50%, 1)'; // rgba(255, 0, 0, 1)
 
+  drawContainer(ctx, polygonColor, initialX, initialY, polygonWidth, polygonHeight, polygonShadowColor, polygonShadowX, polygonShadowY);
+  drawText(ctx, textBaseline, font, textColor, text1, initialX + textIndentX, textIndentY);
+  drawText(ctx, textBaseline, font, textColor, text2, initialX + textIndentX, textIndentY + additionalTextIndentY);
 
   for(var i = 0; i < times.length; i++) {
-    if (names[i] === "Вы") {
-      ctx.fillStyle = "rgba(255, 0, 0, 1)";
-    } else {
-      ctx.fillStyle = "hsl(200, 99%, " + Math.random() * 100 + "%)";
+
+    var bar = {
+      'colorForOthersPlayers': 'hsla(200, 99%, ' + Math.random() * 100 + '%, 1)',
+      'x': initialX + initialIndentForBar + additionalIndentForBar * i,
+      'y': polygonHeight - indentForBarY,
+      'width': barWidth,
+      'indent': - (step * times[i]),
+      'text': '' + Math.floor(times[i]),
+      'textIndent': polygonHeight - indentForBarY - valueIndentY - (step * times[i]),
+      'sign': names[i],
+      'signIndent': polygonHeight - indentForNameY
     }
-    
-    ctx.fillRect(initialX + initialIndentForBar + additionalIndentForBar * i, polygonHeight - indentForBarY, barWidth, - (step * times[i]));
-    ctx.fillStyle = textColor;
-    ctx.fillText("" + Math.floor(times[i]), initialX + initialIndentForBar + additionalIndentForBar * i, polygonHeight - indentForBarY - valueIndentY - (step * times[i]));
-    ctx.fillText(names[i], initialX + initialIndentForBar + additionalIndentForBar * i, polygonHeight - indentForNameY);
+
+    paintBar(ctx, names[i], currentPlayer, currentPlayerColor, bar.colorForOthersPlayers);
+    drawBarAndSignature(ctx, bar.x, bar.y, bar.width, bar.indent, bar.text, textColor, bar.textIndent, bar.sign, bar.signIndent);
   }
 };
+
+
+
+
+
+
